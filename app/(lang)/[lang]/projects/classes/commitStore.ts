a10b6ed2368268f7b46fc8e658f3247d15bd07ef
Sina <sadeghesfahani.sina@gmail.com>
2025-10-15 09:53:06 +0200

    projects and test has been removed

--- app/(lang)/[lang]/projects/classes/commitStore.ts
+++
@@ -1,7 +0,0 @@
-import type { Commit, UUID } from "./interface";
-
-export interface CommitStore {
-    get(id: UUID): Promise<Commit | undefined>;
-    save(projectId: UUID, commit: Commit): Promise<void>;
-    listByProject(projectId: UUID): Promise<Commit[]>;
-}
