d2dd6a0c122c95d4632f263b988d4007955cdc3c
Sina <sadeghesfahani.sina@gmail.com>
2025-10-16 13:47:16 +0200

    make the commit problem fix

--- app/(lang)/[lang]/ai/[projectId]/page.tsx
+++ app/(lang)/[lang]/ai/[projectId]/page.tsx
@@ -11,7 +11,7 @@ export default function ProjectPage() {
         { projectId },
         {
             refetchOnMountOrArgChange: true,
-            refetchOnFocus: true,
+            refetchOnFocus: false,
         }
     );
 
